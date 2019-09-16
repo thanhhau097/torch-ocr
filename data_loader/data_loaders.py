61d4ac17c4447edfc6342aa84e258404cd0b78de
Nguyen Thanh Hau <thanhhau097@gmail.com>
2019-09-16 23:03:45 +0700

    update dataset code

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,23 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+from data_loader.dataset import OCRDataset
 
 
-class MnistDataLoader(BaseDataLoader):
+class OCRDataLoader(BaseDataLoader):
     """
     MNIST data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, json_path, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
+            # transforms.Normalize((0.1307,), (0.3081,))
+            # TODO: resize image to same height
         ])
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.json_path = json_path
+        self.dataset = OCRDataset(data_dir, json_path, trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+if __name__ == '__main__':
+    dataloader = OCRDataLoader()
---
+++ data_loader/dataset.py
@@ -0,0 +1,48 @@
+import os
+import json
+
+import cv2
+from torch.utils.data import Dataset
+
+
+class OCRDataset(Dataset):
+    """Read dataset for OCR"""
+    def __init__(self, data_dir, json_path, transform=None, channels=3):
+        self.data_dir = data_dir
+        self.json_path = json_path
+        self.transform = transform
+        self.channels = channels
+
+        self.image_paths, self.labels = self.__get_image_paths_and_labels(self.get_data_path(json_path))
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.get_data_path(self.image_paths[idx])
+        image = self.read_image(img_path)
+
+        label = self.labels[idx]
+        sample = {"image": image, "label": label}
+        return sample
+
+    def read_image(self, path):
+        if self.channels == 1:
+            img = cv2.imread(path, 0)
+        elif self.channels == 3:
+            img = cv2.imread(path)
+        else:
+            raise ValueError("Number of channels must be 1 or 3")
+
+        return img
+
+    def get_data_path(self, path):
+        return os.path.join(self.data_dir, path)
+
+    def __get_image_paths_and_labels(self, json_path):
+        with open(json_path, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        image_paths = list(data.keys())
+        labels = list(data.values())
+        return image_paths, labels
